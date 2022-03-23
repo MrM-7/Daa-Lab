259788acffc5a807feaf78695de0202243981b3d
Mehul Raj <71964538+MrM-7@users.noreply.github.com>
2022-03-23 10:49:38 +0530

    Bubble Sort

---
+++ new.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int n;
+	cout<<"Enter size: ";
+	cin>>n;
+	
+	int arr[n];
+	
+	cout<<"Enter array: ";
+	for(int i=0; i<n; i++){
+		cin>>arr[i];
+	}
+	
+	// Bubble Sort
+	
+	int comp = 0, swap = 0;
+	
+	for(int i=0; i<n-1; i++){
+		for(int j=0; j<n-1-i; j++){
+			comp++;
+			if(arr[j] > arr[j+1]){
+					swap++;
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+			}
+		}
+	if(swap == 0)
+			break;
+	}
+	
+	cout<<"Sorted array: ";
+	for(int i=0; i<n; i++)
+		cout<<arr[i]<<" ";
+	
+	cout<<endl;
+	
+	cout<<"Comparision : "<<comp<<endl;
+	cout<<"Swapping : "<<swap<<endl;	
+}
