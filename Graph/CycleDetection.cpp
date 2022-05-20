219a2605f97e2e34a0e751f7b9fd05b0b5636d61
Mehul Raj <71964538+MrM-7@users.noreply.github.com>
2022-05-20 21:00:45 +0530

    Update

--- Graph/CycleDetection.cpp
+++ Graph/CycleDetection.cpp
@@ -1,8 +1,3 @@
-
-
-                    /***   Not Completed   ***/
-
-
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -11,10 +6,10 @@ void addEdge(vector<int> adj[], int u, int v){
     adj[v].push_back(u);
 }
 
-
+// Recursive code to check for cycle in graph
 bool cycleDetection(vector<int> adj[], int s, int visited[], int parent){
     visited[s] = 1;
-    bool result;
+    bool result = false;
 
     for(auto x: adj[s]){
         if(visited[x] != 1){
@@ -24,8 +19,6 @@ bool cycleDetection(vector<int> adj[], int s, int visited[], int parent){
 
         else if(x != parent) 
             return true;
-        
-        else return false;
     }
     return result;
 }
@@ -33,42 +26,21 @@ bool cycleDetection(vector<int> adj[], int s, int visited[], int parent){
 int main(){
     int n=5;
     
-    // cout<<"Enter total no. of vertices: ";
-    // cin>>n;
+    cout<<"Enter total no. of vertices: ";
+    cin>>n;
     vector<int> adj[n];
     
-    // for(int i=0;;i++){
-    //     cout<<"Enter edge: ";
-    //     int u,v;
-    //     cin>>u>>v;
-    //     addEdge(adj,u,v);
-    //     cout<<"Quit(Y/N): ";
-    //     char ch;
-    //     cin>>ch;
-    //     if(ch=='Y'||ch=='y') break;
-    // }
-    
-    
-    // addEdge(adj,0,1);
-    // addEdge(adj,1,2);
-    // addEdge(adj,2,3);
-    // addEdge(adj,3,4);
-    // addEdge(adj,4,0);
-
-    // addEdge(adj,0,1);
-    // addEdge(adj,0,4);
-    // addEdge(adj,1,0);
-    // addEdge(adj,1,2);
-    // addEdge(adj,2,1);
-    // addEdge(adj,2,3);
-    // addEdge(adj,3,2);
-    // addEdge(adj,3,4);
-
-    addEdge(adj, 1, 0);
-    addEdge(adj, 0, 2);
-    addEdge(adj, 2, 1);
-    addEdge(adj, 0, 3);
-    addEdge(adj, 3, 4);
+  // For loop for edges input
+    for(int i=0;;i++){
+        cout<<"Enter edge: ";
+        int u,v;
+        cin>>u>>v;
+        addEdge(adj,u,v);
+        cout<<"Quit(Y/N): ";
+        char ch;
+        cin>>ch;
+        if(ch=='Y'||ch=='y') break;
+    }
 
     cout<<"Enter source vertex: ";
     int src;
@@ -78,8 +50,8 @@ int main(){
     bool result = cycleDetection(adj,src,visited,-1);
 
 
-    if(result)  cout<<"Graph contains cycle\n";
-    else        cout<<"Graph doesn't contain cycle\n";
+    if(result)  cout<<"-----Graph contains cycle-----\n";
+    else        cout<<"-----Graph doesn't contain cycle-----\n";
 
     return 0;
 }
